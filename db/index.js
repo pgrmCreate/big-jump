9d40e5cf453b9d40612efceb18aaa69b84796653
pgrmCreate <luccioni.jbaptiste@gmail.com>
2023-02-19 20:51:00 +0100

    update db read

--- db/index.js
+++ db/index.js
@@ -3,5 +3,6 @@ const GameConfigSchema = require('./models/GameConfigSchema');
 
 
 exports.LoadDb = async function () {
-    await mongoose.connect('mongodb://127.0.0.1:27017/explo2');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/explo2');
+    await mongoose.connect('mongodb://127.0.0.1:27017/explo2', { useNewUrlParser: true, useUnifiedTopology: true });
 }
