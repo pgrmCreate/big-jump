e2555c89245c2149c5f7ba6514fc74a6308a4957
pgrmCreate <luccioni.jbaptiste@gmail.com>
2023-02-23 15:19:17 +0100

    db connect log

--- db/index.js
+++ db/index.js
@@ -4,4 +4,5 @@ const GameConfigSchema = require('./models/GameConfigSchema');
 
 exports.LoadDb = async function () {
     await mongoose.connect(`mongodb+srv://luccionijbaptiste:${process.env.DB_PASSWORD}@cluster0.zji75nl.mongodb.net/?retryWrites=true&w=majority`);
+    console.log('db connected');
 }
