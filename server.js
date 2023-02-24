a1abf05bf044085b2ca128e2595d986417791d1e
pgrmCreate <luccioni.jbaptiste@gmail.com>
2023-02-24 16:17:20 +0100

    simple render

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const cookieParser = require("cookie-parser");
 
 //db.LoadDb().catch(err => console.log(err));
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
