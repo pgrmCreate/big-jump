02fbf9259e851a19050c4a0e3381878c46cce4d8
pgrmCreate <luccioni.jbaptiste@gmail.com>
2023-02-25 11:32:50 +0100

    update token in env var

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'Dz99z5a9q6d5z9464fes98fd4sefse6ef9se465fDDRAMD_4dqz');
+        const decodedToken = jwt.verify(token, process.env.TOKEN_AUTH);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
